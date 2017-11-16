574da2607852c47bb35c1fcc7fe45ddfc68ecc36
Yakaweiwei <yswwnmjk@163.com>
2017-11-16 22:06:13 +0800

    加入了南极人的跳跃，攻击与左右移动。发现南极人与地面距离有点远，可能需要调整碰撞体的检测。

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody2D rb;
+    public float movespeed;
+    public float jumpheight;
+    public bool moveright;
+    public bool moveleft;
+    public bool jump;
+
+    // Shoot Parameters
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate = 0.5f; //the frequency of shoot 
+    private float nextFire;
+
+    // Parameters to check whether on ground
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool onGround;
+
+    [HideInInspector]
+    public bool facingRight;
+
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Check whether on ground
+    void FixedUpdate()
+    {
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+        
+    void Update () {
+
+        // Control From keyboard A:shoot, space: jump, left and right arrow: control
+        if (Input.GetKey (KeyCode.LeftArrow)) {
+            rb.velocity = new Vector2 (-movespeed, rb.velocity.y);
+            facingRight = false;
+
+        }
+        if (Input.GetKey (KeyCode.RightArrow)) {
+            rb.velocity = new Vector2 (movespeed, rb.velocity.y);
+            facingRight = true;
+
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (onGround)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpheight);
+            }
+        }
+
+        if (Input.GetKey (KeyCode.A) && Time.time > nextFire) 
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+
+        // Control From Touch
+        if (moveright) {
+            rb.velocity = new Vector2 (movespeed, rb.velocity.y);
+            facingRight = true;
+        }
+
+        if (moveleft) {
+            rb.velocity = new Vector2 (-movespeed, rb.velocity.y);
+            facingRight = false;
+        }
+
+        if (jump)
+        {
+            if (onGround)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpheight);
+            }
+            jump = false;
+        }
+    }
+}
---
+++ Assets/Scripts/SnowBullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowBullet : MonoBehaviour {
+
+    public float speed = 20f;
+	// Use this for initialization
+
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+        Destroy(gameObject, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
