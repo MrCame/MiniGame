8978e22590de738cd9ceaa07d7e3cb479bae53ce
Yakaweiwei <yswwnmjk@163.com>
2017-11-27 17:24:49 +0800

    重新创立游戏工程，加入了人物控制，动画机，场景已经搭建好

---
+++ 未定游戏/Assets/Scripts/Shoot.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour
+{
+    public Rigidbody2D shot;
+    public Transform shotSpawn;
+    public float speed = 10f;               // The speed the rocket will fire at.
+
+    public float fireRate = 0.5f; //the frequency of shoot 
+    private float nextFire = 0f;
+
+    private PlayerController playerCtrl;       // Reference to the PlayerControl script.
+    private Animator anim;                  // Reference to the Animator component.
+
+
+    void Awake()
+    {
+        // Setting up the references.
+        anim = transform.root.gameObject.GetComponent<Animator>();
+        playerCtrl = transform.root.GetComponent<PlayerController>();
+    }
+
+
+    void Update ()
+    {
+        // If the fire button is pressed...
+        if (Input.GetKey (KeyCode.A) && Time.time > nextFire) 
+        {
+            nextFire = Time.time + fireRate;
+            // ... set the animator Shoot trigger parameter and play the audioclip.
+            anim.SetTrigger("WalkShoot");
+            anim.SetTrigger("IdleShoot");
+
+            // If the player is facing right...
+            if(playerCtrl.facingRight)
+            {
+                // ... instantiate the rocket facing right and set it's velocity to the right. 
+                Rigidbody2D bulletInstance = Instantiate(shot, shotSpawn.position, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
+                bulletInstance.velocity = new Vector2(speed, 0);
+            }
+            else
+            {
+                // Otherwise instantiate the rocket facing left and set it's velocity to the left.
+                Rigidbody2D bulletInstance = Instantiate(shot, shotSpawn.position, Quaternion.Euler(new Vector3(0,0,180f))) as Rigidbody2D;
+                bulletInstance.velocity = new Vector2(-speed, 0);
+            }
+        }
+    }
+}
