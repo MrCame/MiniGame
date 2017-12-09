23d77264db0b0aaca69a353f1ca3f3c2628617df
Yakaweiwei <yswwnmjk@163.com>
2017-12-09 13:53:18 +0800

    修改了背景音乐，图鉴界面加入了音乐

---
+++ 未定游戏/Assets/Scripts/DocMusic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DocMusic : MonoBehaviour {
+    //将准备好的MP3格式的声音文件拖入此处  
+    public AudioClip Music;
+    //用于控制声音的AudioSource组件 
+    [HideInInspector] 
+    public AudioSource _DocSource; 
+
+    void Start()  
+    {  
+       //在添加此脚本的对象中添加AudioSource组件，此处为摄像机  
+       _DocSource = this.gameObject.AddComponent<AudioSource>();  
+       //设置循环播放  
+       _DocSource.loop = true;  
+       //设置音量为最大，区间在0-1之间  
+       _DocSource.volume = PlayerPrefs.GetFloat("MusicVolume", 1);  
+       //设置audioClip  
+       _DocSource.clip = Music;  
+       _DocSource.Play();
+    } 
+}
